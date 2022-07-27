632619912bf4b80f1df4ca90060b619031182987
Eric Stokes <letaris@gmail.com>
2022-07-26 23:03:44 -0400

    much progress

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@
 extern crate lazy_static;
 
 mod sysfs;
+use crate::sysfs::{FType, Files, Poller, Fid};
+use futures::{prelude::*, select_biased, channel::mpsc};
 use anyhow::Result;
+use log::{error, warn};
+use fxhash::FxHashMap;
 use netidx::{
     path::Path,
-    publisher::{BindCfg, Id, Publisher, Val},
+    publisher::{BindCfg, DefaultHandle, Id, Publisher, Val, PublishFlags},
+    utils::{Batched, BatchItem},
 };
 use netidx_tools::ClientParams;
-use std::path::PathBuf;
+use std::{collections::HashMap, ops::Bound, path::PathBuf, time::Duration, mem};
 use structopt::StructOpt;
-use tokio;
+use tokio::{self, task};
 
 #[derive(StructOpt, Debug)]
 struct Params {
@@ -34,9 +39,156 @@ struct Params {
     sysfs: PathBuf,
 }
 
+fn netidx_path<P: AsRef<std::path::Path>>(base: &Path, path: P) -> Path {
+    base.append(&path.as_ref().as_os_str().to_string_lossy())
+}
+
+struct Published {
+    published: FxHashMap<Id, Val>,
+    by_fid: FxHashMap<Fid, Id>,
+    advertised: HashMap<Path, (PathBuf, Option<Id>)>,
+    aliased: HashMap<Path, Path>,
+}
+
+impl Published {
+    fn resolve(&mut self, path: &Path) -> Option<&mut (PathBuf, Option<Id>)> {
+        let tgt = self.aliased.get(path).cloned();
+        match tgt {
+            Some(tgt) => self.resolve(&tgt),
+            None => self.advertised.get_mut(path),
+        }
+    }
+    
+    fn advertise(base: &Path, files: &Files, dp: &DefaultHandle) -> Self {
+        let mut t = Published {
+            published: HashMap::default(),
+            by_fid: HashMap::default(),
+            advertised: HashMap::default(),
+            aliased: HashMap::default(),
+        };
+        for (path, typ) in &files.paths {
+            match typ {
+                FType::Directory => (),
+                FType::File => {
+                    let npath = netidx_path(base, path);
+                    if let Err(e) = dp.advertise(npath.clone()) {
+                        warn!("failed to advertise path {}, {}", npath, e)
+                    }
+                    t.advertised.insert(npath, (path.clone(), None));
+                }
+            }
+        }
+        for (src, tgt) in &files.symlinks {
+            match files.resolve(tgt) {
+                Err(e) => {
+                    warn!("bad symlink {:?} -> {:?} {}", src, tgt, e)
+                }
+                Ok((tgt, typ)) => match typ {
+                    FType::File => {
+                        let src_path = netidx_path(base, src);
+                        let tgt_path = netidx_path(base, tgt);
+                        if t.advertised.contains_key(&tgt_path) {
+                            if let Err(e) = dp.advertise(src_path.clone()) {
+                                warn!("failed to advertise path {}, {}", src_path, e)
+                            }
+                            t.aliased.insert(src_path, tgt_path);
+                        } else {
+                            warn!("symlink to missing path {} -> {}", src_path, tgt_path)
+                        }
+                    }
+                    FType::Directory => {
+                        let src_path = netidx_path(base, src);
+                        let tgt_path = netidx_path(base, tgt);
+                        let children = files
+                            .paths
+                            .range::<std::path::Path, (Bound<&std::path::Path>, Bound<&std::path::Path>)>((
+                                Bound::Excluded(tgt),
+                                Bound::Unbounded,
+                            ))
+                            .take_while(|(p, _)| p.starts_with(tgt))
+                            .map(|(p, t)| (p.strip_prefix(tgt), t));
+                        for (p, typ) in children {
+                            let p = match p {
+                                Ok(p) => p,
+                                Err(_) => continue,
+                            };
+                            match typ {
+                                FType::Directory => (),
+                                FType::File => {
+                                    let src_path = netidx_path(&src_path, p);
+                                    let tgt_path = netidx_path(&tgt_path, p);
+                                    if t.advertised.contains_key(&tgt_path) {
+                                        if let Err(e) = dp.advertise(src_path.clone()) {
+                                            warn!("failed to advertise path {}, {}", src_path, e)
+                                        }
+                                        t.aliased.insert(src_path, tgt_path);
+                                    } else {
+                                        warn!("symlink to missing path {} -> {}", src_path, tgt_path)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+            }
+        }
+        t
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::init();
     let opts = Params::from_args();
     let (cfg, auth) = opts.common.load();
+    let timeout = opts.timeout.map(Duration::from_secs);
+    let files = sysfs::Files::new(opts.sysfs)?;
     let publisher = Publisher::new(cfg, auth, opts.bind).await?;
+    let (tx_updates, rx_updates) = mpsc::unbounded();
+    let mut rx_updates = Batched::new(rx_updates, 10_000);
+    let poller = Poller::new(tx_updates);
+    let mut dp = publisher.publish_default(opts.base.clone())?;
+    let mut published = Published::advertise(&opts.base, &files, &dp);
+    let mut updates = publisher.start_batch();
+    loop {
+        select_biased! {
+            (p, reply) = dp.select_next_some() => if let Some((fspath, ref mut id)) = published.resolve(&p) {
+                match id {
+                    Some(id) => match publisher.alias(*id, p.clone()) {
+                        Ok(()) => (),
+                        Err(e) => warn!("failed to alias {}, {}", p, e),
+                    },
+                    None => match poller.start(fspath.clone()).await {
+                        Err(e) => warn!("polling file {} failed {}", p, e),
+                        Ok((fid, v)) => {
+                            let flags = PublishFlags::DESTROY_ON_IDLE;
+                            match publisher.publish_with_flags(flags, p.clone(), v) {
+                                Err(e) => warn!("failed to publish {}, {}", p, e),
+                                Ok(val) => {
+                                    let vid = val.id();
+                                    *id = Some(vid);
+                                    published.published.insert(vid, val);
+                                    published.by_fid.insert(fid, vid);
+                                    let _ = reply.send(());
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            r = rx_updates.select_next_some() => match r {
+                BatchItem::InBatch((fid, v)) => match published.by_fid.get(&fid) {
+                    None => poller.stop(fid)?,
+                    Some(id) => match published.published.get(id) {
+                        None => poller.stop(fid)?,
+                        Some(val) => val.update_changed(&mut updates, v),
+                    }
+                }
+                BatchItem::EndBatch => {
+                    let up = mem::replace(&mut updates, publisher.start_batch());
+                    up.commit(timeout).await
+                }
+            },
+        }
+    }
 }
--- src/sysfs.rs
+++ src/sysfs.rs
@@ -1,21 +1,25 @@
 use anyhow::{bail, Result};
+use futures::{
+    channel::{
+        mpsc::{self, UnboundedSender},
+        oneshot,
+    },
+    prelude::*,
+    select_biased,
+};
 use fxhash::FxHashMap;
 use log::error;
 use netidx::{
     pool::{Pool, Pooled},
-    publisher::{Id, Value},
+    publisher::Value,
 };
 use std::{
-    collections::{BTreeMap, BTreeSet, HashMap},
+    collections::{BTreeMap, HashMap, HashSet},
     ops::{Deref, DerefMut},
     path::{Path, PathBuf},
     time::Duration,
 };
-use tokio::{
-    sync::mpsc::{self, UnboundedSender},
-    task::JoinHandle,
-    time,
-};
+use tokio::{sync::broadcast, task::JoinHandle, time};
 use tokio_uring::{
     self,
     buf::{IoBuf, IoBufMut},
@@ -91,54 +95,88 @@ async fn read_file(file: &Path) -> Result<Buf> {
 
 pub(crate) async fn poll_file(
     file: PathBuf,
-    id: Id,
-    updates: UnboundedSender<(Id, Value)>,
+    id: Fid,
+    updates: UnboundedSender<(Fid, Value)>,
+    first: oneshot::Sender<Value>,
+    mut clock: broadcast::Receiver<()>,
 ) -> Result<()> {
+    let mut first = Some(first);
+    let mut send = |v: Value| -> Result<()> {
+        match first.take() {
+            None => Ok(updates.unbounded_send((id, v))?),
+            Some(first) => {
+                let _ = first.send(v);
+                Ok(())
+            }
+        }
+    };
     loop {
         let data = read_file(&file).await?;
         let data = String::from_utf8_lossy(&**data);
         match data.parse::<i64>() {
-            Ok(i) => updates.send((id, Value::from(i)))?,
+            Ok(i) => send(Value::from(i))?,
             Err(_) => match data.parse::<f64>() {
-                Ok(f) => updates.send((id, Value::from(f)))?,
-                Err(_) => updates.send((id, Value::from(data.into_owned())))?,
+                Ok(f) => send(Value::from(f))?,
+                Err(_) => send(Value::from(data.into_owned()))?,
             },
         }
-        time::sleep(Duration::from_secs(1)).await
+        if let Err(_) = clock.recv().await {
+            break Ok(());
+        }
+    }
+}
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+pub(crate) struct Fid(u64);
+
+impl Fid {
+    fn new() -> Self {
+        use std::sync::atomic::{AtomicU64, Ordering};
+        static NEXT: AtomicU64 = AtomicU64::new(0);
+        Fid(NEXT.fetch_add(1, Ordering::Relaxed))
     }
 }
 
 enum FileReq {
-    StartPolling(Id, PathBuf),
-    StopPolling(Id),
+    StartPolling(Fid, PathBuf, oneshot::Sender<Value>),
+    StopPolling(Fid),
 }
 
-pub(crate) struct Poller(mpsc::UnboundedSender<FileReq>);
+pub(crate) struct Poller(UnboundedSender<FileReq>);
 
 impl Poller {
     async fn run(
-        updates: mpsc::UnboundedSender<(Id, Value)>,
+        updates: mpsc::UnboundedSender<(Fid, Value)>,
         mut req: mpsc::UnboundedReceiver<FileReq>,
     ) -> Result<()> {
-        let mut polling: FxHashMap<Id, JoinHandle<Result<()>>> = HashMap::default();
-        while let Some(req) = req.recv().await {
-            match req {
-                FileReq::StartPolling(id, path) => {
-                    let jh = tokio_uring::spawn(poll_file(path, id, updates.clone()));
-                    polling.insert(id, jh);
+        let (clock, _) = broadcast::channel(3);
+        let mut polling: FxHashMap<Fid, JoinHandle<Result<()>>> = HashMap::default();
+        let mut clock_timer = time::interval(Duration::from_secs(1));
+        loop {
+            select_biased! {
+                _ = clock_timer.tick().fuse() => {
+                    let _ = clock.send(());
                 }
-                FileReq::StopPolling(id) => {
-                    if let Some(jh) = polling.remove(&id) {
-                        jh.abort();
+                r = req.select_next_some() => match r {
+                    FileReq::StartPolling(id, path, first) => {
+                        let clock = clock.subscribe();
+                        let jh = tokio_uring::spawn(
+                            poll_file(path, id, updates.clone(), first, clock)
+                        );
+                        polling.insert(id, jh);
+                    }
+                    FileReq::StopPolling(id) => {
+                        if let Some(jh) = polling.remove(&id) {
+                            jh.abort();
+                        }
                     }
                 }
             }
         }
-        Ok(())
     }
 
-    pub(crate) fn new(updates: mpsc::UnboundedSender<(Id, Value)>) -> Self {
-        let (tx_req, rx_req) = mpsc::unbounded_channel();
+    pub(crate) fn new(updates: mpsc::UnboundedSender<(Fid, Value)>) -> Self {
+        let (tx_req, rx_req) = mpsc::unbounded();
         std::thread::spawn(move || {
             tokio_uring::start(async move {
                 if let Err(e) = Self::run(updates, rx_req).await {
@@ -149,34 +187,72 @@ impl Poller {
         Self(tx_req)
     }
 
-    pub(crate) fn start(&self, id: Id, path: PathBuf) -> Result<()> {
-        match self.0.send(FileReq::StartPolling(id, path)) {
-            Ok(()) => Ok(()),
+    pub(crate) async fn start(&self, path: PathBuf) -> Result<(Fid, Value)> {
+        let id = Fid::new();
+        let (tx, rx) = oneshot::channel();
+        match self.0.unbounded_send(FileReq::StartPolling(id, path, tx)) {
+            Ok(()) => match rx.await {
+                Ok(v) => Ok((id, v)),
+                Err(_) => bail!("failed to poll file"),
+            },
             Err(_) => bail!("file poller dead"),
         }
     }
 
-    pub(crate) fn stop(&self, id: Id) -> Result<()> {
-        match self.0.send(FileReq::StopPolling(id)) {
+    pub(crate) fn stop(&self, id: Fid) -> Result<()> {
+        match self.0.unbounded_send(FileReq::StopPolling(id)) {
             Ok(()) => Ok(()),
             Err(_) => bail!("file poller dead"),
         }
     }
 }
 
+#[derive(Clone, Copy)]
+pub(crate) enum FType {
+    File,
+    Directory,
+}
+
 pub(crate) struct Files {
-    paths: BTreeSet<PathBuf>,
-    symlinks: BTreeMap<PathBuf, PathBuf>,
+    pub(crate) paths: BTreeMap<PathBuf, FType>,
+    pub(crate) symlinks: BTreeMap<PathBuf, PathBuf>,
 }
 
 impl Files {
     pub(crate) fn new(path: PathBuf) -> Result<Self> {
         let mut t = Self {
-            paths: BTreeSet::default(),
-            symlinks: BTreeMap::default()
+            paths: BTreeMap::default(),
+            symlinks: BTreeMap::default(),
         };
         t.walk(path)?;
         Ok(t)
+    }
+
+    fn resolve_<'a, 'b: 'a>(
+        &'a self,
+        target: &'b PathBuf,
+        cycle: usize,
+    ) -> Result<(&'a PathBuf, FType)> {
+        match self.symlinks.get(target) {
+            Some(target) => {
+                let cycle = cycle + 1;
+                if cycle > 256 {
+                    bail!("too many levels of symbolic links")
+                }
+                self.resolve_(target, cycle)
+            }
+            None => match self.paths.get(target) {
+                None => bail!("broken link"),
+                Some(ft) => Ok((target, *ft)),
+            },
+        }
+    }
+
+    pub(crate) fn resolve<'a, 'b: 'a>(
+        &'a self,
+        target: &'b PathBuf,
+    ) -> Result<(&'a PathBuf, FType)> {
+        self.resolve_(target, 0)
     }
 
     fn walk(&mut self, path: PathBuf) -> Result<()> {
@@ -189,9 +265,9 @@ impl Files {
             for ent in fs::read_dir(&path)? {
                 self.walk(ent?.path())?
             }
-            self.paths.insert(path);
+            self.paths.insert(path, FType::Directory);
         } else if st.is_file() {
-            self.paths.insert(path);
+            self.paths.insert(path, FType::File);
         }
         Ok(())
     }
